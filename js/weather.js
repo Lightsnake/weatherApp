edb382db2c25c55393dc60be31076e7e343c321b
Lightsnake <andrew.tsan@yahoo.com>
2019-06-20 21:17:46 -0400

    initial commit

---
+++ js/weather.js
@@ -0,0 +1,57 @@
+window.addEventListener('load', ()=>{
+    let long;
+    let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
+    let temperatureSection = document.querySelector('.temperature-section');
+    const temperatureSpan = document.querySelector('.temperature-section span');
+
+    if (navigator.geolocation){
+         navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const proxy ='https://cors-anywhere.herokuapp.com/';
+        
+            const api= `${proxy}https://api.darksky.net/forecast/b338331c3ae615891faf9bd19652e4c9/${lat},${long}`;
+            fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then (data => {
+                const {temperature, summary, icon } = data.currently; 
+                //Set DOM Elements from the API
+                temperatureDegree.textContent = temperature;
+                temperatureDescription.textContent = summary;
+                locationTimezone.textContent = data.timezone.replace(/_/g, " ");
+                    //Formula for Celsius
+                    let celsius = (temperature - 32) * (5/9);
+                    //Set Icon
+                    setIcons(icon, document.querySelector('.icon'));
+                    //Change Temperature to Celsius/Farenheit
+                    temperatureSection.addEventListener('click', ()=>{
+                        if(temperatureSpan.textContent === "F"){
+                            temperatureSpan.textContent="C";
+                            temperatureDegree.textContent = Math.floor(celsius);
+                            
+                        }else{
+                            temperatureSpan.textContent="F";
+                            temperatureDegree.textContent=temperature;
+                        }
+                    });
+            })
+            
+         });  
+    }else{
+        h1.textContext="Please enable geolocation to use this";
+    }
+    
+    function setIcons(icon, iconID){
+        const skycons = new Skycons({color: "white"});
+        const currentIcon = icon.replace (/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+    }
+   
+});
